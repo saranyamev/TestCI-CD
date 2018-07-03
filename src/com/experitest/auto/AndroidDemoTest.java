7f22511cb0010e26150d20d73edd8b67e638b36e
Saranya <Saranya@192.168.11.192>
2018-07-03 17:17:25 +0400

    Config.yml

--- src/com/experitest/auto/AndroidDemoTest.java
+++ src/com/experitest/auto/AndroidDemoTest.java
@@ -1,57 +1,47 @@
 package com.experitest.auto;
-
 //package <set your test package>;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.logging.Level;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.android.AndroidKeyCode;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.By;
+import org.testng.annotations.*;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
 
 public class AndroidDemoTest {
-	  public static final String USERNAME = "YOUR_USERNAME";
-	  public static final String ACCESS_KEY = "FAED11E078BB400FAC2E6123F6FD89AF";
-	  public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
-	 
     private String reportDirectory = "reports";
     private String reportFormat = "xml";
-    private String testName = "Untitled";
+    private String testName = "Android Test1";
+    private String accessKey = "eyJ4cC51IjoxNDUxNDUsInhwLnAiOjE0NTE0NCwieHAubSI6Ik1UVXpNRFl4TXpZd01ESTJOQSIsImFsZyI6IkhTMjU2In0.eyJleHAiOjE4NDU5NzM2MDAsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.KTQcweVJFYfTyQpITDV9kLv2-oQ7COnK5NB0Y9LK1Rs";
     protected AndroidDriver<AndroidElement> driver = null;
 
     DesiredCapabilities dc = new DesiredCapabilities();
     
     @BeforeMethod
     public void setUp() throws MalformedURLException {
-    	        dc.setCapability("reportDirectory", reportDirectory);
-    	        dc.setCapability("reportFormat", reportFormat);
-    	        dc.setCapability("testName", testName);
-    	        dc.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-    	        //dc.setCapability("testobjectApiKey", "FAED11E078BB400FAC2E6123F6FD89AF");
-    	        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.example.circleci.circlecidemo2");
-    	        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".MainActivity");
-    	        //driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), dc);
-    	        driver.setLogLevel(Level.INFO);
+        dc.setCapability("reportDirectory", reportDirectory);
+        dc.setCapability("reportFormat", reportFormat);
+        dc.setCapability("testName", testName);
+        dc.setCapability("accessKey", accessKey);
+        dc.setCapability(MobileCapabilityType.UDID, "0117114300883901");
+        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
+        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
+        dc.setCapability("instrumentApp", true);
+        driver = new AndroidDriver<>(new URL("https://demo.experitest.com:443/wd/hub"), dc);
+        driver.setLogLevel(Level.INFO);
     }
 
-    @Test
+
     public void testUntitled() {
-        //new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class='android.widget.TextView']")));
-        driver.findElement(By.xpath("//*[@id='smallLabel']")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Home']")));
-      
-        //driver.findElement(By.xpath("//*[@text='Notifications']")).getAttribute("value");
-        driver.findElement(By.xpath("//*[@text='Notifications']")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Notifications']")));
+        driver.findElement(By.xpath("//*[@text='Dashboard']")).click();
+        // driver.pressKeyCode(AndroidKeyCode.HOME);
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Dashboard']")));
     }
 
     @AfterMethod
